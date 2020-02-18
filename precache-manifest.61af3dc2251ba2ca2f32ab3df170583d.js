002056c6a280eb78523a9f1b21dc94ca5883d916
Nelson Lim <30296067+nelsonlim96@users.noreply.github.com>
2020-02-18 17:49:20 +0800

    Updates

---
+++ precache-manifest.61af3dc2251ba2ca2f32ab3df170583d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd611dc65afb8470abc7e963c6fdc18",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "366e0a8993cee1e65071",
+    "url": "/TicTacToe/static/css/main.ed255a59.chunk.css"
+  },
+  {
+    "revision": "932036ae7f3b19988e42",
+    "url": "/TicTacToe/static/js/2.42e2b5bd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/TicTacToe/static/js/2.42e2b5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "366e0a8993cee1e65071",
+    "url": "/TicTacToe/static/js/main.3e112d0c.chunk.js"
+  },
+  {
+    "revision": "4619cd138c914eec619d",
+    "url": "/TicTacToe/static/js/runtime-main.f135e921.js"
+  }
+]);
